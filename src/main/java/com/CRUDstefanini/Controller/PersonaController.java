0813f5916a2e735444bcf632cf09ee10fb7e7e95
Fernando Tapia Arcos <fernandotapiaarcos@outlook.es>
2024-05-20 10:06:27 -0500

    crud complete

---
+++ src/main/java/com/CRUDstefanini/Controller/PersonaController.java
@@ -0,0 +1,49 @@
+package com.CRUDstefanini.Controller;
+
+import com.CRUDstefanini.Entity.PersonaEntity;
+import com.CRUDstefanini.Service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/personas")
+public class PersonaController {
+
+    @Autowired
+    private PersonaService personaService;
+
+    @GetMapping
+    public List<PersonaEntity> getPersonas(){
+        return personaService.getAllPersonas();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonaEntity> getPersona(@PathVariable(name = "id") Long id){
+        Optional<PersonaEntity> personaEntity = personaService.getPersonaById(id);
+        return personaEntity.map(persona -> new ResponseEntity<>(persona, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<PersonaEntity> createPersona(@RequestBody PersonaEntity personaEntity){
+        PersonaEntity persona = personaService.savePersona(personaEntity);
+        return new ResponseEntity<>(persona, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PersonaEntity> updatePersona(@PathVariable(name = "id") Long id, @RequestBody PersonaEntity persona){
+        PersonaEntity updatePersona = personaService.updatePersona(id, persona);
+        return new ResponseEntity<>(updatePersona, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePersona(@PathVariable(name = "id") Long id){
+        personaService.deletePersona(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/CRUDstefanini/Entity/PersonaEntity.java
@@ -0,0 +1,33 @@
+package com.CRUDstefanini.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "persona")
+public class PersonaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idPersona;
+
+    @Column(name = "nombre", columnDefinition = "VARCHAR(255)")
+    String nombre;
+
+    @Column(name = "apellido", columnDefinition = "VARCHAR(255)")
+    String apellido;
+
+    @Column(name = "fechaNac",columnDefinition = "DATE")
+    String fechaNac;
+
+    @Column(name = "dni", columnDefinition = "INT")
+    int dni;
+
+}
